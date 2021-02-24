9b608563ba9812508ea781c39080d4118a89fe3d
dlgustn1245 <dlgustn1245@naver.com>
2021-02-24 22:29:43 +0900

    Add player's movement and Machine gun

---
+++ GunsSurvival/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed; 
+    public Rigidbody2D rb2d = null;
+    public Boundary boundary;
+
+    float horizontal, vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rb2d.position;
+        position.x += speed * horizontal * Time.deltaTime;
+        position.y += speed * vertical * Time.deltaTime;
+
+        position.x = Mathf.Clamp(position.x, boundary.xMin, boundary.xMax);
+        position.y = Mathf.Clamp(position.y, boundary.yMin, boundary.yMax);
+
+        rb2d.MovePosition(position);
+    }
+}
---
+++ GunsSurvival/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject laser;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MachineGun());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator MachineGun()
+    {
+        while (true)
+        {
+            Instantiate(laser, new Vector2(transform.position.x, transform.position.y) + Vector2.up * 0.5f, Quaternion.identity);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
