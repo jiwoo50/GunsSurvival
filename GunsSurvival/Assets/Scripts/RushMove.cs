e8c5675cbcd2bd6323b40284153b637409dcb444
jiwoo50 <10622jiwoo@gmail.com>
2021-02-25 03:07:29 +0900

    Implement the enemy's movement pattern
    Implement tracking enemy and rush enemy

---
+++ GunsSurvival/Assets/Scripts/RushMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RushMove : MonoBehaviour
+{
+    public GameObject player;
+    public float movePower=5.0f;
+
+    Rigidbody2D rigidbody2D;
+    bool isRush = true;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Rush();
+        if (Mathf.Abs(transform.position.y) >= 4.89f || Mathf.Abs(transform.position.x) >= 2.95f)
+        {
+            rigidbody2D.velocity = Vector3.zero;
+            isRush = true;
+            Rush();
+        }
+    }
+    void Rush()
+    {
+        if (isRush)
+        {
+            Vector3 dir = player.transform.position - transform.position;
+            dir.Normalize();
+            rigidbody2D.AddForce(dir*movePower, ForceMode2D.Impulse);
+            isRush = false;
+        }
+    }
+}
---
+++ GunsSurvival/Assets/Scripts/TrackingEnemyMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingEnemyMove : MonoBehaviour
+{
+    public GameObject player;
+    public float movePower = 5.0f;
+
+    Rigidbody2D rigidbody2D;
+    float searchTime = 0.7f;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (searchTime <= 0.0f)
+        {
+            Rush();
+            searchTime = 0.7f;
+        }
+        if (searchTime > 0.0f)
+        {
+            searchTime -= Time.deltaTime;
+        }
+        if (Mathf.Abs(transform.position.y) >= 4.89f || Mathf.Abs(transform.position.x) >= 2.95f)
+        {
+            Rush();
+        }
+    }
+    void Rush()
+    {
+        rigidbody2D.velocity = Vector3.zero;
+        Vector3 dir = player.transform.position - transform.position;
+        dir.Normalize();
+        rigidbody2D.AddForce(dir * movePower, ForceMode2D.Impulse);
+    }
+}
