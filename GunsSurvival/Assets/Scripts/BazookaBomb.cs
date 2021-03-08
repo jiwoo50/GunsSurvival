023aa343cb69f8428932d5e28664c7549fb3f36f
jiwoo50 <10622jiwoo@gmail.com>
2021-03-09 01:43:11 +0900

    Add Bazooka sprite and ShotGun sprite

---
+++ GunsSurvival/Assets/Scripts/BazookaBomb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BazookaBomb : MonoBehaviour
+{
+    //Bazooka bomb
+    public float speed;
+
+    Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rb2d.position;
+        position.y += speed * Time.deltaTime;
+        rb2d.MovePosition(position);
+    }
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ GunsSurvival/Assets/Scripts/MachinegunBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachinegunBullet : MonoBehaviour
+{
+    //Machine Gun bullet
+    public GameObject Limit;
+    public float speed;
+    
+    Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector2 position = rb2d.position;
+        position.y += speed * Time.deltaTime;
+
+        rb2d.MovePosition(position);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("MachineGunLimit"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
