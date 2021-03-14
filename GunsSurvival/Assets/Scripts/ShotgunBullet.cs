081648f2ae26643ba576ccbd90e0b2258e1acf91
jiwoo50 <10622jiwoo@gmail.com>
2021-03-15 03:20:03 +0900

    Implement the spread of Shotgun bullets
    Transform the angle of the bullet according to the number of bullets.

--- GunsSurvival/Assets/Scripts/ShotgunBullet.cs
+++ GunsSurvival/Assets/Scripts/ShotgunBullet.cs
@@ -5,21 +5,39 @@ using UnityEngine;
 public class ShotgunBullet : MonoBehaviour
 {
     //shotgun bullet
-    public float speed;
+    public GameObject bulletSpawn;
+    public GameObject bullet;
+    public GameObject player;
 
-    Rigidbody2D rb2d;
+    public float bulletSpeed;
+    public int numOfBullets;
+
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+
     }
-    void FixedUpdate()
+    void Update()
     {
-        Vector2 position = rb2d.position;
-        position.y += speed * Time.deltaTime;
-        rb2d.MovePosition(position);
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Shot();
+        }
     }
-    void OnBecameInvisible()
+    void Shot()
     {
-        Destroy(gameObject);
+        for (int i = 0; i < numOfBullets; i++)
+        {
+            //총알 토큰 생성
+            GameObject tempBullet = (GameObject)Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            //총알에 물리 부여
+            Rigidbody2D tempBulletRB = tempBullet.GetComponent<Rigidbody2D>();
+            float spreadAngle = -10+20*i/numOfBullets;//총알의 각도
+            float x = bulletSpawn.transform.position.x - player.transform.position.x;
+            float y = bulletSpawn.transform.position.y - player.transform.position.y;
+            float rotateAngle = spreadAngle + (Mathf.Atan2(y, x) * Mathf.Rad2Deg)*3/2;//발사되는 각 계산
+            Vector2 MovementDirection = new Vector2(Mathf.Cos(rotateAngle * Mathf.Deg2Rad), Mathf.Sin(rotateAngle * Mathf.Deg2Rad)).normalized;
+            tempBulletRB.AddForce(MovementDirection * bulletSpeed, ForceMode2D.Impulse);
+            Destroy(tempBullet, 5.0f);
+        }
     }
 }
