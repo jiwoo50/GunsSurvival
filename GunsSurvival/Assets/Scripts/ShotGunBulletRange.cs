3b54d4626b76ad4c2761a1627e686bb737bd7495
jiwoo50 <10622jiwoo@gmail.com>
2021-03-15 19:54:16 +0900

    Add ShotGun's range

---
+++ GunsSurvival/Assets/Scripts/ShotGunBulletRange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGunBulletRange : MonoBehaviour
+{
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("bulletLimit"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
