8bc35f35a6c059eb0b3825716225c60a7031d51e
dlgustn1245 <dlgustn1245@naver.com>
2021-07-02 17:49:43 +0900

    Implement enemies spawn.

--- GunsSurvival/Assets/Scripts/SpawnMgr.cs
+++ GunsSurvival/Assets/Scripts/SpawnMgr.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class SpawnMgr : MonoBehaviour
 {
-    //public float spawnWait = 1.5f;
+    public GameObject[] enemies;
+    public Transform[] points;
 
-    //public int maxEnemy = 15;
+    public float spawnDelay = 1.5f;
+
+    public int maxEnemy = 15;
+
+    bool flag = true;
 
     void Start()
     {
+        points = GameObject.Find("SpawnPoints").GetComponentsInChildren<Transform>();
+    }
 
+    void Update()
+    {
+        if(GameController.Instance.startSpawn && flag)
+        {
+            StartCoroutine(SpawnEnemy());
+            flag = false;
+        }
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (!GameController.Instance.gameOver)
+        {
+            int enemyCnt = (int)GameObject.FindGameObjectsWithTag("Rush").Length + (int)GameObject.FindGameObjectsWithTag("Tracking").Length;
+            if (enemyCnt <= maxEnemy)
+            {
+                yield return new WaitForSeconds(spawnDelay);
+                int pos_idx = Random.Range(1, points.Length);
+                int enemy_idx = Random.Range(0, enemies.Length);
+                Instantiate(enemies[enemy_idx], points[pos_idx].position, Quaternion.identity);
+            }
+            else yield return null;
+        }
     }
 }
